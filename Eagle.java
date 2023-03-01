b1ab1bd017b4104a4151aa4d47ec4e85e4f52919
Joshua Dupin <joshua35260@gmail.com>
2023-03-01 12:33:31 +0100

    done

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,46 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+
+
+    @Override
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.printf("%s takes off in the sky.%n", this.getName());
+        }
+    }
+
+    @Override
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude + meters, 120);
+            System.out.printf("%s flies upward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public void glide() {
+        System.out.printf("%s glides into the air.%n", this.getName());
+    }
+
+    @Override
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude - meters, 9);
+            System.out.printf("%s flies downward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    @Override
+    public void land() {
+        if (this.flying && this.altitude == 0) {
+            System.out.printf("%s is landed.%n", this.getName());
+        } else {
+            System.out.printf("%s is too high, it can't get landed.%n", this.getName());
+        }
     }
 }
---
+++ Fly.java
@@ -0,0 +1,12 @@
+public interface Fly {
+
+  int ascend(int meters);
+
+  int descend(int meters);
+  
+  void takeOff();
+
+  void glide();
+
+  void land();
+}
